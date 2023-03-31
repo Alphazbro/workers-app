3544a1503bffa96bd2e2c74a63e1c6f2c2a0e364
alphazbro <115604432+Alphazbro@users.noreply.github.com>
2023-03-31 06:56:26 +0000

    added database

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for,flash, redirect,Request
+
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='JHAHAHAHAHSk'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+db=SQLAlchemy(app)
+
+
+class user(db.Model):
+  id=db.column(db.Integer, primary_key=True)
+  username=db.column(db.String, unique=True, nullable=False)
+  email=db.column(db.String, unique=True, nullable=False)
+  password=db.column(db.String, nullable=False)
+  posts=db.relationship('posts', backref='author', lazy=True)
+
+  def __repr__(self):
+    return f"user('{self.username}','{self.email}')"
+
+class post(db.model):
+  id=db.column(db.Integer, primary_key=True)
+  title=db.column(db.String, nullable=False)
+  date_posted=db.column(db.DateTime, nullable=False, default=datetime.utcnow)
+  content=db.column(db.Text, nullable=False)
+  user_id=db.column(db.Integer,db.ForeignKey('user.id'), nullable=False)
+  def __repr__(self):
+    return f"post('{self.title}','{self.date_posted}')"
+  
+  
 
 from forms import registrationform, loginform
 
@@ -29,7 +56,7 @@ def about():
 def register():
   form=registrationform()
   if form.validate_on_submit():
-    flash('account created successfully','success')
+    flash('account created successfully',category='success')
     return redirect(url_for('home'))
   return render_template('register.html',form=form)
 
